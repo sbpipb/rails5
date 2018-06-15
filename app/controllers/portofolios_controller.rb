546ea7e2f9bb4f49e0c87266b81510e260fb7893
jamacmang <julianmacmang@gmail.com>
2018-06-15 20:31:50 +0800

    create porto

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -7,4 +7,23 @@ class PortofoliosController < ApplicationController
     # return render: json: @portofolios
   end
 
+  def create
+    @portofolio_item = Portofolio.new create_params
+
+    respond_to do |format|
+      if @portofolio_item.save
+        format.html { redirect_to portofolios_path, notice: 'Your portofolio item is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def new
+    @portofolio_item = Portofolio.new
+  end
+
+  def create_params
+    params.require(:portofolio).permit(:title, :subtitle, :body)
+  end
 end
