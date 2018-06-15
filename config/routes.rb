3a383220f1e9afb839c1fee4a53f1bda459a550e
jamacmang <julianmacmang@gmail.com>
2018-06-15 16:36:51 +0800

    batch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   resources :blogs
+
+  # get '/', to: 'pages#about', as: 'about_me'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
