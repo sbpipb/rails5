f02654e538efd38a0603a8064b1f0071eb72cbea
jamacmang <julianmacmang@gmail.com>
2018-06-15 22:01:49 +0800

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   resources :categories
-  resources :portofolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portofolios, except: [:show]
+
+
+  get 'portofolio/:id', to: 'portofolios#show', as: 'portofolio_show'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
 
   root to: 'pages#home'
-
-  # get '/', to: 'pages#about', as: 'about_me'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
