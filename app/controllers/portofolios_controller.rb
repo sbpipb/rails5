6299ac938175ef9fa3181c3284a84dcce5abd6b8
jamacmang <julianmacmang@gmail.com>
2018-06-15 20:10:51 +0800

    portofolio view

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -1,2 +1,10 @@
 class PortofoliosController < ApplicationController
+
+  before_action :find_portofolio, only: :show
+
+  def index
+    @portofolio_items = Portofolio.all
+    # return render: json: @portofolios
+  end
+
 end
